7a3bfdd915085cf1cc10b66a657d49c468bf9fde
marco mastio <marco.mastio@gmail.com>
2019-06-22 21:25:25 +0200

    First Commit Implementato movimento di base per l'arrampicata (rigidbody-based).

---
+++ Assets/Scripts/Climbing/GripManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripManager : MonoBehaviour
+{
+    public Pull lController;
+    public Pull rController;
+    public Rigidbody playerBody;
+
+    void FixedUpdate()
+    {
+        if ((lController.isGrabGripPressed && lController.canGrip) || (rController.isGrabGripPressed && rController.canGrip))
+        {
+            UseGravity(false);
+
+            if (lController.isGrabGripPressed && lController.canGrip)
+            {
+                lController.TryGrab();
+            }
+
+            if (rController.isGrabGripPressed && rController.canGrip)
+            {
+                rController.TryGrab();
+            }
+        }
+        else
+        {
+            UseGravity(true);
+        }
+    }
+
+
+    private void UseGravity(bool grav)
+    {
+        playerBody.useGravity = grav;
+        playerBody.isKinematic = !grav;
+    }
+
+}
---
+++ Assets/Scripts/Climbing/Pull.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Pull : MonoBehaviour
+{
+    public SteamVR_Action_Boolean grab; //Grab Pinch is the trigger, select from inspecter
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
+
+    public Vector3 prevPosition;
+
+    [SerializeField]
+    public bool canGrip;
+    public bool isGrabGripPressed;
+
+    public Rigidbody playerBody;
+
+    #region Initialization
+    void OnEnable()
+    {
+        if (grab != null)
+        {
+            grab.AddOnChangeListener(GripButtonClick, inputSource);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (grab != null)
+        {
+            grab.RemoveOnChangeListener(GripButtonClick, inputSource);
+        }
+    }
+
+    void Start()
+    {
+         prevPosition = this.transform.localPosition;
+    }
+
+
+    private void LateUpdate()
+    {
+        prevPosition = this.transform.position;
+    }
+    #endregion
+
+    //questa funzione, viene chiamata quando viene premuto o rilasciato il tasto di grip
+    private void GripButtonClick(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool pressed)
+    {
+        isGrabGripPressed = pressed;
+
+        if (!pressed)
+        {
+            AddReleaseVelocity();
+        }
+    }
+    
+    public void TryGrab()
+    {
+        if (canGrip)
+        {
+            //playerBody.MovePosition(playerBody.transform.position + (prevPosition - transform.position));
+
+            playerBody.transform.position += prevPosition - transform.position;
+        }
+    }
+    
+    private void AddReleaseVelocity()
+    {
+        ////se rilascio il bottone, dopo essermi attaccato a qualcosa, aggiungo una spinta
+        if (canGrip)
+        {
+
+            playerBody.velocity = (prevPosition - transform.position) / Time.deltaTime * 100;
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canGrip = true;
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        canGrip = false;
+    }
+}
